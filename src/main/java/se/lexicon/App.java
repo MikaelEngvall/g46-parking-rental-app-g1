9a460c60b73746e187e1d7e504f043e6ccad6e84
MikaelEngvall <mikael@engvall.org>
2023-09-04 14:48:30 +0200

    Tried out some stuff

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,8 +1,7 @@
 package se.lexicon;
 
 import se.lexicon.data.CustomerDao;
-import se.lexicon.data.impl.CustomerDaoImpl;
-import se.lexicon.data.impl.ReservationDaoImpl;
+import se.lexicon.data.impl.*;
 import se.lexicon.model.Customer;
 import se.lexicon.model.ParkingSpot;
 import se.lexicon.model.Reservation;
@@ -19,16 +18,20 @@ public class App {
     public static void main(String[] args) {
 
         CustomerDao customerDao = CustomerDaoImpl.getInstance();
-//        ParkingSpotDaoImpl parkingSpotDao = ParkingSpotDaoImpl.getInstance();
-//        ParkingLotDaoImpl parkingLotDao = ParkingLotDaoImpl.getInstance();
-//        VehicleDaoImpl vehicleDao = VehicleDaoImpl.getInstance();
+        ParkingSpotDaoImpl parkingSpotDao = ParkingSpotDaoImpl.getInstance();
+        ParkingLotDaoImpl parkingLotDao = ParkingLotDaoImpl.getInstance();
+        VehicleDaoImpl vehicleDao = VehicleDaoImpl.getInstance();
         ReservationDaoImpl reservationDao = ReservationDaoImpl.getInstance();
 
         Customer customer = new Customer("Test Testson", "123456");
         customerDao.create(customer);
         ParkingSpot parkingSpot = new ParkingSpot(12, false);
+        parkingSpotDao.create(parkingSpot);
         System.out.println("Parking spot " + parkingSpot.getSpotNumber() + " is now" + (parkingSpot.isOccupied() ? " occupied" : " free"));
+//        ParkingLot parkingLot = new ParkingLot(123,new HashMap<>()); //todo ???
+//        System.out.println(parkingLot.getAreaCode());
         Vehicle vehicle = new Vehicle("ABC123", CAR);
+        vehicleDao.create(vehicle);
         Reservation reservation = new Reservation(LocalTime.of(0,1), LocalTime.of(23,59), customer, parkingSpot, vehicle);
         reservationDao.create(reservation); // Here we reserve the spot and state it occupied
         System.out.println("Reserving parking spot : " + reservation.getParkingSpot().getSpotNumber());
